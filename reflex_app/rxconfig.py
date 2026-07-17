902e5e3f8b43a8bc5d1c8681fd734f66a3ec17d0
Luca Borrelli <l.borrelli@reply.it>
2026-07-17 14:32:50 +0200

    test

--- reflex_app/rxconfig.py
+++ reflex_app/rxconfig.py
@@ -4,7 +4,7 @@ from reflex_base.plugins.sitemap import SitemapPlugin
 config = rx.Config(
     app_name="aws_simulator",
     backend_port=8000,
-    backend_host="127.0.0.1",
+    backend_host="0.0.0.0",  # Ascolta su tutte le interfacce di rete
     telemetry_enabled=False,
     show_built_with_reflex=False,
     disable_plugins=[SitemapPlugin],
